2b35c14929654eab77e3a9bb683eaaf139e1176c
Fede Aguilera <fedeaguilera.15@gmail.com>
2023-01-12 07:03:19 -0300

    Homeworck 04-Bucles

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -33,7 +33,7 @@ function obtenerSaludo(nombre) {
    // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
    // Ejemplo: "Martin" ---> "Hola Martin!"
    // Tu código:
-   let stringSaludo = 'Hola ' + nombre + '!';
+   let stringSaludo = 'Hola '+ nombre + '!';
    return stringSaludo;
 }
 
